9769182c3628b8009d7654122e140062a6021b6d
Natasha Setiadi <natset456@gmail.com>
2020-01-27 16:44:46 -0500

    got color sensor to detect color and make encoder go certain position

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,165 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.subsystems.SetEncodertoColor;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  SetEncodertoColor encoderColor = new SetEncodertoColor();
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
+  /**
+   * A Rev Color Sensor V3 object is constructed with an I2C port as a 
+   * parameter. The device will be automatically initialized with default 
+   * parameters.
+   */
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+
+  /**
+   * A Rev Color Match object is used to register and detect known colors. This can 
+   * be calibrated ahead of time or during operation.
+   * 
+   * This object uses a simple euclidian distance to estimate the closest match
+   * with given confidence range.
+   */
+  private final ColorMatch colorMatcher = new ColorMatch();
+
+  /**
+   * Note: Any example colors should be calibrated as the user needs, these
+   * are here as a basic example.
+   */
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  @Override
+  public void robotInit() {
+    colorMatcher.addColorMatch(kBlueTarget);
+    colorMatcher.addColorMatch(kGreenTarget);
+    colorMatcher.addColorMatch(kRedTarget);
+    colorMatcher.addColorMatch(kYellowTarget);    
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    encoderColor.init();
+    encoderColor.errorSum = 0;
+    encoderColor.lastError = 0;
+    encoderColor.lastTimestamp = Timer.getFPGATimestamp();
+    // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    encoderColor._motor.setSelectedSensorPosition(0, 0, 1000);
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    SmartDashboard.putNumber("encoder value", encoderColor._motor.getSelectedSensorPosition(0) * encoderColor.kDriveTick2Feet);
+
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    Color detectedColor = colorSensor.getColor();
+
+    /**
+     * Run the color match algorithm on our detected color
+     */
+    String colorString;
+    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+      encoderColor.blue();
+
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+      encoderColor.red();
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+      encoderColor.green();
+
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+      encoderColor.yellow();
+
+    } else {
+      colorString = "Unknown";
+    }
+    SmartDashboard.putString("Color Detected", colorString);
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SetEncodertoColor.java
@@ -0,0 +1,169 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SetEncodertoColor extends SubsystemBase {
+  /**
+   * Creates a new SetEncodertoColor.
+   */
+  public static WPI_TalonSRX _motor = new WPI_TalonSRX(2);
+
+  public static final double kP = 0.5;
+  public static final double kI = 0.4;
+  public static final double kD = 0.1;
+  public static final double iLimit = 1;
+
+  public static double setpoint = 0;
+  public static double errorSum = 0;
+  public static double lastTimestamp = 0;
+  public static double lastError = 0;
+  public static final double kDriveTick2Feet = 1.0 / 128 * 6 * Math.PI / 12;
+
+  public SetEncodertoColor() {
+
+  }
+  public void init(){
+    _motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    _motor.configOpenloopRamp(0.2);
+    _motor.configClosedloopRamp(0.2);
+    // Talon setup. Setting up + and - output for max and nominal.
+    // These should never really change
+    _motor.configNominalOutputForward(0, 0);
+    _motor.configNominalOutputReverse(0, 0);
+    _motor.configPeakOutputForward(1, 0);
+    _motor.configPeakOutputReverse(-1, 0);
+    _motor.setSensorPhase(true);
+    _motor.setInverted(true);
+  }
+  public void blue() {
+    setpoint = 90; //degree to set encoder to
+    
+    
+
+    // get sensor position
+    double sensorPosition = _motor.getSelectedSensorPosition(0) * kDriveTick2Feet;
+
+    // calculations
+    double error = setpoint - sensorPosition;
+    double dt = Timer.getFPGATimestamp() - lastTimestamp;
+
+    if (Math.abs(error) < iLimit) {
+      errorSum += error * dt;
+
+    }
+    double errorRate = (error - lastError) / dt;
+
+    double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
+
+    // output to motors
+    _motor.set(outputSpeed);
+
+    // update last- variables
+    lastTimestamp = Timer.getFPGATimestamp();
+    lastError = error;
+  }
+
+  public void red(){
+    setpoint = 25;
+   
+    
+
+    // get sensor position
+    double sensorPosition = _motor.getSelectedSensorPosition(0) * kDriveTick2Feet;
+
+    // calculations
+    double error = setpoint - sensorPosition;
+    double dt = Timer.getFPGATimestamp() - lastTimestamp;
+
+    if (Math.abs(error) < iLimit) {
+      errorSum += error * dt;
+
+    }
+    double errorRate = (error - lastError) / dt;
+
+    double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
+
+    // output to motors
+    _motor.set(outputSpeed);
+
+    // update last- variables
+    lastTimestamp = Timer.getFPGATimestamp();
+    lastError = error;
+
+  }
+  public void yellow(){
+    setpoint = 50;
+   
+    
+
+    // get sensor position
+    double sensorPosition = _motor.getSelectedSensorPosition(0) * kDriveTick2Feet;
+
+    // calculations
+    double error = setpoint - sensorPosition;
+    double dt = Timer.getFPGATimestamp() - lastTimestamp;
+
+    if (Math.abs(error) < iLimit) {
+      errorSum += error * dt;
+
+    }
+    double errorRate = (error - lastError) / dt;
+
+    double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
+
+    // output to motors
+    _motor.set(outputSpeed);
+
+    // update last- variables
+    lastTimestamp = Timer.getFPGATimestamp();
+    lastError = error;
+
+  }
+  public void green(){
+    setpoint=100;
+   
+    
+
+    // get sensor position
+    double sensorPosition = _motor.getSelectedSensorPosition(0) * kDriveTick2Feet;
+
+    // calculations
+    double error = setpoint - sensorPosition;
+    double dt = Timer.getFPGATimestamp() - lastTimestamp;
+
+    if (Math.abs(error) < iLimit) {
+      errorSum += error * dt;
+
+    }
+    double errorRate = (error - lastError) / dt;
+
+    double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
+
+    // output to motors
+    _motor.set(outputSpeed);
+
+    // update last- variables
+    lastTimestamp = Timer.getFPGATimestamp();
+    lastError = error;
+
+  }
+
+  public void encoderSet(double setpoint){
+ 
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
